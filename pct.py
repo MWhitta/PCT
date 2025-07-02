2e3164b5f65e7c7d008bb069f265e9ca6613e399
mwhittaker <mwhittaker@lbl.gov>
2025-07-02 15:38:00 -0700

    pct.py base functionality for loading and storing data

---
+++ pct.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Data():
+    """ object for holding data, metadata, and analysis
+    """
+
+
+
+class Dataloader():
+    """ load PCT data and structure it for processing
+    """
+
+    def __init__(self):
+        """ stuff happens
+        """
+
+    def load(self, ext='.csv'):
+        """ load data and store in a Data object
+        """
+
